3e115e1a985b12ae9f1ad9d7e90096a570b59107
vyytx <dobeender@gmail.com>
2022-06-10 01:08:13 +0800

    initialize

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Nametag from './lib/Namtag.svelte'
+	// import logo from './assets/svelte.png'
+	// import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+	<navbar />
+	<Nametag />
+</main>
+
+<style lang="scss">
+	@import 'node_modules/normalize.css/normalize.css';
+
+	// :root {
+		
+	// }
+
+	html, body {
+		position: absolute;
+		top: 0px;
+		width: 100%;
+		height: 100%;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/lib/Namtag.svelte
@@ -0,0 +1,20 @@
+<div class="holder">
+    <p>vyytx / {roles[roleId]}</p>
+</div>
+
+<script>
+    let roles = ['Coding', 'Designing', 'Arranging']
+    let roleId = 0;
+    setInterval(() => {
+        roleId++;
+        if(roleId >= roles.length) roleId = 0;
+    }, 1000)
+</script>
+
+<style lang="scss">
+    .holder {
+        width: 80vw;
+        height: 100vh;
+    }
+
+</style>
